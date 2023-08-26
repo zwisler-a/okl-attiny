b34cc2f146aad56e0379fdc4805f06f63aa52c05
Alexander Zwisler <alex-z-@web.de>
2023-08-27 01:47:31 +0200

    rev1: runtimetest

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include "tinysnore.h"
 
-const char *startupLEDPattern = "10101";
-const char *unknownCommandPattern = "1010111";
+const char *startupLEDPattern = "10111";
+const char *sleepLEDPattern = "1010111";
+const char *unknownCommandPattern = "101010111";
 
-const byte rxPin = 3;
-const byte txPin = 4;
-const byte ledPin = 1;
-const byte espPin = 2;
+const char END_OF_COMMAND = ';';
+
+const byte rxPin = PIN_PB3;
+const byte txPin = PIN_PB4;
+const byte ledPin = LED_BUILTIN;
+const byte espPin = PIN_PB0;
+const byte sensorPin = A1;
 
 SoftwareSerial softwareSerial(rxPin, txPin); // RX, TX
 
@@ -19,7 +24,7 @@ void playSequence(const char *pattern) {
         } else if (pattern[i] == '0') {
             digitalWrite(ledPin, LOW);
         }
-        delay(500);
+        delay(100);
     }
     digitalWrite(ledPin, LOW);
 }
@@ -30,38 +35,51 @@ void setup() {
     pinMode(txPin, OUTPUT); // Set TX pin as OUTPUT
     pinMode(ledPin, OUTPUT);
     pinMode(espPin, OUTPUT);
-    digitalWrite(espPin, HIGH);
+    pinMode(sensorPin, INPUT);
+    while (Serial.available()) { Serial.read(); }
     playSequence(startupLEDPattern);
+    digitalWrite(espPin, HIGH);
 }
 
-void enterSleep(long duration) {
-    digitalWrite(espPin, LOW);
-    for (long i = 0; i < duration / 2; i++) {
-        digitalWrite(ledPin, HIGH);
-        delay(100);
-        digitalWrite(ledPin, LOW);
-        delay(1900);
+void enterSleep(long duration_in_sec) {
+    if (duration_in_sec <= 1) {
+        return;
     }
+    playSequence(sleepLEDPattern);
+    digitalWrite(espPin, LOW);
+    snore((duration_in_sec * 1000) - 2500);
     digitalWrite(espPin, HIGH);
+    playSequence(sleepLEDPattern);
+    while (Serial.available()) { Serial.read(); }
 }
 
 void loop() {
     if (softwareSerial.available() > 0) {
-        digitalWrite(ledPin, HIGH);
-        String cmd = softwareSerial.readStringUntil('\n');
-        digitalWrite(ledPin, LOW);
-        if (cmd.startsWith("S")) {
-            int duration = atol(cmd.substring(2).c_str());
+        String cmd = softwareSerial.readStringUntil(END_OF_COMMAND);
+        if (cmd.startsWith("S:")) {
+            String durationStr = cmd.substring(2);
+            if (durationStr.length() < 1) {
+                playSequence(unknownCommandPattern);
+            }
+            int duration = atol(durationStr.c_str());
             softwareSerial.print("#S:");
-            softwareSerial.println(duration);
+            softwareSerial.print(duration);
+            softwareSerial.print(END_OF_COMMAND);
             enterSleep(duration);
             return;
         }
+
         if (cmd.startsWith("H")) {
-            softwareSerial.println("#H");
+            softwareSerial.print("#H:");
+            softwareSerial.print(analogRead(sensorPin));
+            softwareSerial.print(END_OF_COMMAND);
             return;
         }
-        playSequence(unknownCommandPattern);
 
+        playSequence(unknownCommandPattern);
+        softwareSerial.print("!");
+        softwareSerial.print(cmd);
+        softwareSerial.print(END_OF_COMMAND);
+        while (Serial.available()) { Serial.read(); }
     }
 }
